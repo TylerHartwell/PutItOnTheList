ff3a6c9ada81291acf82314a25ed599499de36c6
TylerHartwell <thartwell37@gmail.com>
2023-06-22 16:01:53 -0700

    highlight item toggle

--- index.js
+++ index.js
@@ -1,12 +1,9 @@
 //TO DO
 //add undo function
-//add quantity option
-//add user and time/date added signature?
-//right click to edit item?
 //name, save, and load list?
 
 import { initializeApp } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-app.js"
-import { getDatabase, ref, push, onValue, remove } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-database.js"
+import { getDatabase, ref, push, onValue, remove, set } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-database.js"
 
 const appSettings = {
     databaseURL: "https://playground-3bec0-default-rtdb.firebaseio.com/"
@@ -14,7 +11,9 @@ const appSettings = {
 
 const app = initializeApp(appSettings)
 const database = getDatabase(app)
-const shoppingListInDB = ref(database, "shoppingList")
+const shoppingListName = "shoppingList"
+const shoppingListInDB = ref(database, shoppingListName)
+
 
 const inputFieldEl = document.getElementById("input-field")
 const addButtonEl = document.getElementById("add-button")
@@ -30,24 +29,18 @@ inputFieldEl.addEventListener("keyup", function(e) {
 addButtonEl.addEventListener("click", function() {
     let inputValue = inputFieldEl.value
     if(inputValue !== "") {
-        push(shoppingListInDB, inputValue)
-    
+        let item = {itemName: inputValue, itemHighlighted: false}
+        push(shoppingListInDB, item)
         clearInputFieldEl()
     }
-    
 })
 
 onValue(shoppingListInDB, function(snapshot) {
     if (snapshot.exists()) {
         let itemsArray = Object.entries(snapshot.val())
-    
         clearShoppingListEl()
-        
         for (let i = 0; i < itemsArray.length; i++) {
             let currentItem = itemsArray[i]
-            let currentItemID = currentItem[0]
-            let currentItemValue = currentItem[1]
-            
             appendItemToShoppingListEl(currentItem)
         }    
     } else {
@@ -55,33 +48,38 @@ onValue(shoppingListInDB, function(snapshot) {
     }
 })
 
+function appendItemToShoppingListEl(item) {
+    let itemID = item[0]
+    let itemValue = item[1].itemName
+    let itemHighlighted = item[1].itemHighlighted
+    let newEl = document.createElement("li")
+    newEl.textContent = itemValue
+   
+    newEl.addEventListener("dblclick", function() {
+        let exactLocationOfItemInDB = ref(database, `${shoppingListName}/${itemID}`)
+        console.log(exactLocationOfItemInDB)
+        remove(exactLocationOfItemInDB)
+    })
+    newEl.addEventListener("click", function() {
+        set(ref(database, `${shoppingListName}/${itemID}/itemHighlighted`), !itemHighlighted)
+        applyHighlightStatus(item, newEl)
+    })
+    shoppingListEl.append(newEl)
+    applyHighlightStatus(item, newEl)
+}
+
+function applyHighlightStatus(item, newEl) {
+    if(item[1].itemHighlighted) {
+        newEl.style.borderColor = "#00FF00"
+    } else {
+        newEl.style.borderColor = "transparent"
+    }
+}
+
 function clearShoppingListEl() {
     shoppingListEl.innerHTML = ""
 }
 
 function clearInputFieldEl() {
     inputFieldEl.value = ""
-}
-
-function appendItemToShoppingListEl(item) {
-    let itemID = item[0]
-    let itemValue = item[1]
-    
-    let newEl = document.createElement("li")
-    
-    newEl.textContent = itemValue
-    //let isHighlighted = false
-    //need to make item values objects with a text property and an isHighlighted property instead of just single strings as values
-    
-    newEl.addEventListener("dblclick", function() {
-        let exactLocationOfItemInDB = ref(database, `shoppingList/${itemID}`)
-        
-        remove(exactLocationOfItemInDB)
-    })
-    
-    // newEl.addEventListener("click", function() {
-    //     change border color to green or transparent
-    // })
-    
-    shoppingListEl.append(newEl)
 }
